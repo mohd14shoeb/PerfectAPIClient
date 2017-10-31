c84968f4d1d6284070bcbd00d16bac88495ca974
Sven Tiigi <sven.tiigi@gmail.com>
2017-10-31 16:01:02 +0100

    Added os check

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,33 +1,39 @@
 #! /usr/bin/env bash
 
-# System Inforamation
-lsb_release -a
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    # System Inforamation
+    lsb_release -a
 
-# Current openssl version
-openssl version -a
+    # Current openssl version
+    openssl version -a
 
-# Update openssl
-sudo add-apt-repository ppa:0k53d-karl-f830m/openssl -y
-sudo apt-get update -q
-sudo apt-get install openssl -y
+    # Update openssl
+    sudo add-apt-repository ppa:0k53d-karl-f830m/openssl -y
+    sudo apt-get update -q
+    sudo apt-get install openssl -y
 
-# Updated openssl version
-openssl version -a
+    # Updated openssl version
+    openssl version -a
 
-# Current curl version
-curl --version
+    # Current curl version
+    curl --version
 
-# Update curl
-sudo apt-get build-dep curl -y
-mkdir ~/curl
-cd ~/curl
-wget http://curl.haxx.se/download/curl-7.50.2.tar.bz2
-tar -xvjf curl-7.50.2.tar.bz2
-cd curl-7.50.2
-./configure
-make
-sudo make install
-sudo ldconfig
+    # Update curl
+    sudo apt-get build-dep curl -y
+    mkdir ~/curl
+    cd ~/curl
+    wget http://curl.haxx.se/download/curl-7.50.2.tar.bz2
+    tar -xvjf curl-7.50.2.tar.bz2
+    cd curl-7.50.2
+    ./configure
+    make
+    sudo make install
+    sudo ldconfig
 
-# Updated curl version
-curl --version
+    # Updated curl version
+    curl --version
+else
+
+fi
+
+
