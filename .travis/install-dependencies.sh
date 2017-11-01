e2b5322ea8395f70bbb3f2a95309a41542470434
Sven Tiigi <sven.tiigi@gmail.com>
2017-11-01 20:39:39 +0100

    Updated install dependencies script

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,21 +1,9 @@
 #! /usr/bin/env bash
 
-# System Inforamation
-lsb_release -a
-
-# Current openssl version
-openssl version -a
-
 # Update openssl
 sudo add-apt-repository ppa:0k53d-karl-f830m/openssl -y
 sudo apt-get update -q
 sudo apt-get install openssl -y
-
-# Updated openssl version
-openssl version -a
-
-# Current curl version
-curl --version
 
 # Update curl
 sudo apt-get build-dep curl -y
@@ -28,6 +16,3 @@ cd curl-7.50.2
 make
 sudo make install
 sudo ldconfig
-
-# Updated curl version
-curl --version
