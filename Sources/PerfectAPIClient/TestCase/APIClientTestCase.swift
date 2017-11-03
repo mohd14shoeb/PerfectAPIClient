691b33a82c47dd641d9ae4733c68fcbf90eb9d75
Sven Tiigi <sven.tiigi@gmail.com>
2017-11-03 23:07:42 +0100

    Updated visibility

--- Sources/PerfectAPIClient/TestCase/APIClientTestCase.swift
+++ Sources/PerfectAPIClient/TestCase/APIClientTestCase.swift
@@ -9,17 +9,17 @@ import XCTest
 import SwiftEnv
 
 /// Base APIClientTestCase for unit testing APIClient
-public class APIClientTestCase: XCTestCase {
+open class APIClientTestCase: XCTestCase {
     
     /// Setup method called before the invocation of each test method in the class.
-    public override func setUp() {
+    open override func setUp() {
         super.setUp()
         // Set isRunning APIClient unit tests to true
         SwiftEnv.isRunningAPIClientUnitTests = true
     }
     
     /// Teardown method called after the invocation of each test method in the class.
-    public override func tearDown() {
+    open override func tearDown() {
         super.tearDown()
         // Set isRunning APIClient unit tests to false
         SwiftEnv.isRunningAPIClientUnitTests = false
