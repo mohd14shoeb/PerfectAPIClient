8072a77ad891a650d8c0d0a22f817ad4d00499d3
Sven Tiigi <sven.tiigi@gmail.com>
2018-01-09 23:24:18 +0100

    Improved APIClientError Cases

--- Sources/PerfectAPIClient/Models/APIClientError.swift
+++ Sources/PerfectAPIClient/Models/APIClientError.swift
@@ -5,15 +5,25 @@
 //  Created by Sven Tiigi on 09.01.18.
 //
 
-/// APIClient Error
+
+/// APIClientError
 ///
-/// - failed: The APIClient failed
+/// - mappingFailed: The mapping failed
+/// - badResponseStatus: Retrieved bad response status
+/// - connectionFailed: The connection failed
 public enum APIClientError: Error {
-    /// APIClient failed with reason and response
-    case failed(
+    
+    /// APIClient failed on mapping
+    case mappingFailed(
         reason: String,
         response: APIClientResponse
     )
+    
+    /// APIClient did retrieved bad response status
+    case badResponseStatus(
+        response: APIClientResponse
+    )
+    
     /// The connection failed
     case connectionFailed(
         error: Error,
@@ -23,10 +33,13 @@ public enum APIClientError: Error {
     /// The localized APIClientError Description
     public var localizedDescription: String {
         switch self {
-        case .failed(reason: let reason, response: let response):
+        case .mappingFailed(reason: let reason, response: let response):
             return "\(reason) | Response: \(response)"
+        case .badResponseStatus(response: let response):
+            return "Retrieved bad response code: \(response.status.code) | Response: \(response)"
         case .connectionFailed(error: let error, request: let request):
             return "\(error.localizedDescription) | Request: \(request)"
         }
     }
+    
 }
